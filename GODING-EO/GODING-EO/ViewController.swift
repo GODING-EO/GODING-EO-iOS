b4b858c7e40c2e1472c210c0e3cec7075d190e71
강한별 <onestar0712@gmail.com>
2022-09-29 09:58:52 +0900

    ㅇㅇ

---
+++ GODING-EO/GODING-EO/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GODING-EO
+//
+//  Created by 강한별 on 2022/09/29.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+
+}
+
---
+++ GODING-EO/GODING-EOUITests/GODING_EOUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  GODING_EOUITestsLaunchTests.swift
+//  GODING-EOUITests
+//
+//  Created by 강한별 on 2022/09/29.
+//
+
+import XCTest
+
+class GODING_EOUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
